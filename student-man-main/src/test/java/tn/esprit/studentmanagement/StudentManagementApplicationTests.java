19f685a9f0d7c542688d6bbcd062cae0be8cae1e
yessinecss <yessine.jmal@esprit.tn>
2025-12-15 13:51:09 +0100

    Initial commit: Student Management DevOps project with Spring Boot, Kubernetes, Docker, and SonarQube

---
+++ student-man-main/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package tn.esprit.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class StudentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
